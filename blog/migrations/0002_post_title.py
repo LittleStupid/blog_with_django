8ecf310f03f9ba5bea659f954693ba79033525b6
LittleStupid <lttlstpd@live.com>
2017-01-09 11:03:42 +0800

    update post admin page

---
+++ blog/migrations/0002_post_title.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='title',
+            field=models.CharField(default='title', max_length=250),
+            preserve_default=False,
+        ),
+    ]
