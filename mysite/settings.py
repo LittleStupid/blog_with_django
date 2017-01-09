bd48a795332e53caad15b5813869d83e665872b1
LittleStupid <lttlstpd@live.com>
2017-01-09 15:26:56 +0800

    send email

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_HOST_USER = os.environ["DJANGO_MAIL"]
+EMAIL_HOST_PASSWORD = os.environ["DJANGO_PASSWORD"]
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
